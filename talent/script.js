07849abf351575e7bfa6b27bc163d8375f80ce7e
ledai94 <brian3dcnbo>
2025-01-10 12:31:12 +0700

    add talent project

---
+++ talent/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.navbar-toggler').click(function() {
+
+        $('#overlay-content').toggleClass('active');
+    });
+
+    // jQuery methods go here...
+
+});
